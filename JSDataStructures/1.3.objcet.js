59f43789a03aa5baf4bd7442f6b1e5bb6fff4982
gitgitcode <yahoolichao@gmail.com>
2015-09-09 23:36:24 +0800

    对象和面向对象编程 1.3

--- JSDataStructures/1.3.objcet.js
+++ JSDataStructures/1.3.objcet.js
@@ -1,3 +1,37 @@
 /**
  * Created by chao on 15/9/9.
  */
+
+function Checking(amount){
+    this.balance = amount; //type
+    this.deposit = deposit ;//function
+    this.widthraw = widthraw;
+    this.toString = toString;
+}
+
+function deposit(amount){
+    this.balacne += amount;
+}
+
+function widthraw(amount){
+    if(amount <= this.balance){
+        this.balance -= amount;
+    }
+    if(amount > this.balance ){
+        console.log("Insuffcienc funds");
+    }
+}
+
+function toString(){
+    return "Balance:"+this.balance
+}
+
+
+var account = new Checking(500);
+
+account.deposit(1000)
+console.log(account.toString());
+account.widthraw(750);
+console.log(account.toString())
+account.widthraw(800)
+console.log(account.toString())
